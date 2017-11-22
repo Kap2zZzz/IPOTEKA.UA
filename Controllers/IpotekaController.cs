9dcee5577610f2f0e16403d9214d96efc8cc947c
Kap2zZzz <33594793+Kap2zZzz@users.noreply.github.com>
2017-11-22 19:37:23 +0200

    ipo3

--- Controllers/IpotekaController.cs
+++ Controllers/IpotekaController.cs
@@ -20,38 +20,47 @@ namespace IPOTEKA.UA.Controllers
         {
             ViewBag.Page = "Ipoteka";
             var model = new Application();
+            model.Termin = 12;
+            model.CreditSum = 500000;
             ViewBag.dicProducts = MainHelp.dicProducts();
             ViewBag.dicSchems = MainHelp.dicSchems();
             return View(model);
         }
 
         [HttpPost]
-        public ActionResult Index(int Termin, decimal CreditSum, string ProductType, string Schema)
+        public ActionResult Index(int Termin, decimal CreditSum, string ProductType, string Schema, Application a)
         {
-            //var products = _db.Products.Where(x => x.Name == ProductType);
-            var banks = _db.Banks.ToList();
-            //_db.Database.Connection.Close();
-
-            List<IpotekaData> resView = new List<IpotekaData>();
-
-            //b.Products
-
-            foreach (Bank b in banks)
-            {
-                foreach (Product p in b.Products.Where(x => x.Name == ProductType))
-                {
-                    IpotekaData temp = new IpotekaData();
-                    temp.Bank = b.Name;
-                    temp.Rate = p.Rate.ToString();
-                    temp.Commission = p.Commission.ToString();
-                    Calculation.GetResults(CreditSum, Termin, p.Rate, true, out temp.MMP, out temp.RealRate, out temp.EffectiveRate);
-                    resView.Add(temp);
-                }
-            }
-
+            ViewBag.Page = "Ipoteka";
             ViewBag.dicProducts = MainHelp.dicProducts();
-            ViewBag.Res = resView;
-            return View();
+
+            if ((ProductType == null) || (ProductType == string.Empty))
+            {
+                ModelState.AddModelError("ProductType", "Поле [Продукт] не вибрано!");
+                return View(a);
+            }
+            else
+            {
+                var banks = _db.Banks.ToList();
+
+                List<IpotekaData> resView = new List<IpotekaData>();
+
+                foreach (Bank b in banks)
+                {
+                    foreach (Product p in b.Products.Where(x => x.Name == ProductType))
+                    {
+                        IpotekaData temp = new IpotekaData();
+                        temp.Bank = b.Name;
+                        temp.Rate = p.Rate.ToString();
+                        temp.Commission = p.Commission.ToString();
+                        Calculation.GetResults(CreditSum, Termin, p.Rate, true, out temp.MMP, out temp.RealRate, out temp.EffectiveRate);
+                        resView.Add(temp);
+                    }
+                }
+
+                ViewBag.Res = resView;
+
+                return View(a);
+            }
         }
 
     }
